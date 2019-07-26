1e689adc8981e9aa23a414728aac771f918669e8
Loskore <sakylax@yandex.ru>
2019-07-26 11:53:16 +0600

    залил свежее изменения

--- controllers/General.js
+++ controllers/General.js
@@ -6,11 +6,12 @@ let config = require('../config');
 
 //Обработчик метода ToSend
 export var toSend = async function(METHOD_NAME, PARAMETERS, ACCESS_TOKEN, V) {
-/**
- * url: 'https://api.vk.com/method/' + 
-        METHOD_NAME + '?' + PARAMETERS + '&access_token=' + ACCESS_TOKEN +
-        '&v=' + V
- */
+ /*
+ https://oauth.vk.com/authorize?client_id=7071471&display=page&redirect_uri=&scope=status&response_type=token&v=5.52
+
+ https://oauth.vk.com/blank.html#access_token=49bd962b34f4cf7340aae39657d84663c48cf95b79b351f16cf7b166c6413076c22543be873051e6c54d9&expires_in=86400&user_id=335342956
+  */
+
     let customurl = config.get('api_vk_url') + METHOD_NAME + '?' + PARAMETERS + '&access_token=' + ACCESS_TOKEN + '&v=' + V;
 
     request.get(customurl, { json: true }, (err, res, body) => {
--- server.js
+++ server.js
@@ -2,8 +2,6 @@
 import express from 'express';
 import morgan from 'morgan';
 import path from 'path';
-import fs from 'fs';
-import bodyParser from 'body-parser';
 
 //Импортируем настройки сервера;
 var config = require('./config');
@@ -15,10 +13,7 @@ import * as basiccontroller from './controllers/General';
 var app = express();
 
 //Настройки сервера перед первым запуском
-app.use(bodyParser.json()); //req.body
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev')); // log every request to the console
-process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
 // Конфигурируем фабрику маршрутизации
 require('./approutes/routes')(app);
@@ -30,13 +25,7 @@ server.listen(config.get('port'), function ()
 {
     console.log(`Server running at http://${config.get('hostname')}:${config.get('port')}/`);
     console.log('Run test method!');
-    //console.log(`${config.get('api_vk_url')}${'status.get'}?group_id=${config.get('gid')}'&access_token='${config.get('token')}'&v='${config.get('version_API')}`);
     //Отправка запроса
-    //basiccontroller.toSend("status.get", "group_id="+config.get('gid'), config.get('token'), config.get('version_API'));
-    basiccontroller.toSend("users.get", "user_ids=213254345", config.get('token'), config.get('version_API')); //WORK !!!!
+    basiccontroller.toSend("status.get", "group_id="+config.get('gid'), config.get('token'), config.get('version_API'));
+    //basiccontroller.toSend("users.get", "user_ids=213254345", config.get('token'), config.get('version_API')); //WORK !!!!
 });
-
-//DIMAS
-//"token" : "7a214ca8531508a5ec865135a988f76948c28158c40952558ba7747c61f132c3312d31db9becd5d0d34af",
-//NIKITA
-//"token" : "1ed49f6afd7c51f6867152c104568db5b62813413608992a0d48d879e9c21eb959031ebc644751e72fede",
